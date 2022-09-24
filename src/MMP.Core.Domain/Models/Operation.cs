f714476c251a92f58fcffac93d59e9c7601e560e
Bruno <brunosalmeida@outlook.com>
2022-09-24 22:10:51 +0100

    + Operation domain

---
+++ src/MMP.Core.Domain/Models/Operation.cs
@@ -0,0 +1,68 @@
+﻿using MMP.Core.Domain.Enums;
+
+namespace MMP.Core.Domain.Models;
+
+public class Operation
+{
+    public Operation(Guid operationId, Guid accountId, decimal value, OperationType operationType,
+        OperationCategory operationCategory, string description, Guid? externalId = null)
+    {
+        OperationId = operationId;
+        AccountId = accountId;
+        Value = value;
+        OperationType = operationType;
+        OperationCategory = operationCategory;
+        ExternalId = externalId;
+        Description = (string.IsNullOrWhiteSpace(description) && OperationCategory == OperationCategory.None)
+            ? throw new ArgumentNullException(nameof(description))
+            : description;
+    }
+
+    public Guid OperationId { get; }
+    public Guid AccountId { get; }
+    public Guid? ExternalId { get; }
+    public decimal Value { get; private set; }
+    public OperationType OperationType { get; private set; }
+    public OperationCategory OperationCategory { get; private set; }
+    public string Description { get; private set; }
+
+    public void ChangeOperationValue(decimal value)
+    {
+        if (OperationType != OperationType.Deposit)
+        {
+            Value = value * -1;
+        }
+        else
+        {
+            Value = value;
+        }
+    }
+
+    public void ChangeDescription(string description)
+    {
+        if (string.IsNullOrWhiteSpace(description) && OperationCategory == OperationCategory.None)
+            throw new Exception("Description can not be empty when category is none");
+
+        Description = description;
+    }
+
+    public void ChangeOperationCategory(OperationCategory operationCategory)
+    {
+        if (string.IsNullOrWhiteSpace(Description) && operationCategory == OperationCategory.None)
+            throw new Exception("Operation category can not be none when description is empty");
+            
+
+        OperationCategory = operationCategory;
+    }
+
+    public void ChangeOperationType(OperationType operationType)
+    {
+        if (operationType == OperationType.Investment && OperationCategory != OperationCategory.Investment)
+            throw new Exception("Operation category must be a Investment");
+
+        if (operationType != OperationType.Deposit)
+            Value = Value * -1;
+
+        OperationType = operationType;
+    }
+}
---
+++ test/MMP.Core.Domain.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+using Xunit;
+
+[assembly: AssemblyTrait("Domain.Models.Operation", "Unit")]
